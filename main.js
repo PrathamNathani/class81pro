d977b793f83e7ab7b7aaeed70b1268a16c054a95
PrathamNathani <73383584+PrathamNathani@users.noreply.github.com>
2021-04-16 12:33:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+ctx.begainPath();
+ctx.strokeStyle="grey";
+ctx.linewidth = 4 ;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.begainPath();
+ctx.strokeStyle="blue";
+ctx.linewidth = 5 ;
+ctx.rect(250 , 210 , 40 , 0 , 2 *Math.PI);
+ctx.stroke();
+
+ctx.begainPath();
+ctx.strokeStyle="black";
+ctx.linewidth = 5 ;
+ctx.rect(350 , 210 , 40 , 0 , 2 *Math.PI);
+ctx.stroke();
+
+ctx.begainPath();
+ctx.strokeStyle="red";
+ctx.linewidth = 5 ;
+ctx.rect(460 , 210 , 40 , 0 , 2 *Math.PI);
+ctx.stroke();
+
+ctx.begainPath();
+ctx.strokeStyle="green";
+ctx.linewidth = 5 ;
+ctx.rect(555 , 210 , 40 , 0 , 2 *Math.PI);
+ctx.stroke();
